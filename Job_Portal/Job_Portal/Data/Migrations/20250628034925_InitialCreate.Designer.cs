ebc6ad33247773449a4523f983974420248af41f
phuoc-hai-2 <daophuochai2004@gmail.com>
2025-06-28 10:51:24 +0700

    no fix

--- Job_Portal/Job_Portal/Data/Migrations/20250628034925_InitialCreate.Designer.cs
+++ Job_Portal/Job_Portal/Data/Migrations/20250628034925_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using Job_Portal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Job_Portal.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250628034925_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
